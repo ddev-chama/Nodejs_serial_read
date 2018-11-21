19a1c07e309522d1566f7b5f8bcd5f6f86e53781
Chama kongsang <ddev.chama@gmail.com>
2018-11-21 11:17:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-var serialport = require("serialport");
-var Serialport = serialport.SerialPort;
-var portName = process.argv[2];
-
-var myPort = new SerialPort(portName,{
-	baudRate:9600,
-	parser:serialport.parsers.readline("\r\n");
-})
-myPort.on('open',onOpen);
-myPort.on('data',onOpen);
-
-function onOpen(){
-	console.log("open Connection");
-}
-function onData(data){
-	console.log("on Data"+data);
-}
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/tty-usbserial1', {
+  baudRate: 9600
+});
+port.on('data', function (data) {
+  console.log('Data:', data.toString());
+});
